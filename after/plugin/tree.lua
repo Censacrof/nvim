04719962466bfd3e5e2abdc3c10fac763c56c59f
Francesco Galisi <francesco.galisi@imagicle.com>
2023-10-02 10:32:51 +0200

    Focus file in tree view

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -2,7 +2,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeOpen)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>ff", vim.cmd.NvimTreeFindFile)
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
