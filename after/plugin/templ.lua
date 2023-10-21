0f0b058b3cf6a0b2446553a978ee0e016e064be5
Francesco Galisi <francesco.galisi@imagicle.com>
2023-10-22 01:04:49 +0200

    Add auto formatting for templ

--- after/plugin/templ.lua
+++ after/plugin/templ.lua
@@ -8,3 +8,18 @@ treesitter_parser_config.templ = {
 }
 
 vim.treesitter.language.register('templ', 'templ')
+
+-- Format current buffer using LSP.
+vim.api.nvim_create_autocmd(
+  {
+    -- 'BufWritePre' event triggers just before a buffer is written to file.
+    "BufWritePre"
+  },
+  {
+    pattern = {"*.templ"},
+    callback = function()
+      -- Format the current buffer using Neovim's built-in LSP (Language Server Protocol).
+      vim.lsp.buf.format()
+    end,
+  }
+)
