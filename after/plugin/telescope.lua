85b33b9feb40d0bb7bae45bf073fe702030d3842
Francesco Galisi <francesco.galisi@imagicle.com>
2023-10-25 17:03:32 +0200

    Add go to reference shortcut

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {}) 
 
+vim.keymap.set('n', 'gr', function() builtin.lsp_references() end, { noremap = true, silent = true })
+
