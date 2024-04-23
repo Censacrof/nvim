44d6f5dcb59c175ab01588e41fc9252910ccaaef
Francesco Galisi <francesco.galisi@imagicle.com>
2024-04-23 18:49:47 +0200

    Improve telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,9 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Telescope: find file" })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: find git file" })
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Telescope: find files (respects .gitignore)" })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: find git files" })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Telescope: live grep" })
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = "Telescope: find help" })
 
 vim.keymap.set('n', 'gr', function() builtin.lsp_references() end,
 	{ desc = "Telescope: find references", noremap = true, silent = true })
