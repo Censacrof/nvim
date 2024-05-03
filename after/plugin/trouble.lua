d053bef59adeeb226d2b57c937658025e9ea348e
Francesco Galisi <francesco.galisi@imagicle.com>
2024-05-03 14:59:53 +0200

    Set focus=true by default for trouble.nvim

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,4 +1,6 @@
-require("trouble").setup()
+require("trouble").setup({
+	focus = true
+})
 
 vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
 
