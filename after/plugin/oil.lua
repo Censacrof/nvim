3a7e182e94ebb9c7fe0c1166e2e659428a853abe
Francesco Galisi <galisifrancesco@gmail.com>
2024-05-08 00:31:53 +0200

    Improve oil keymaps

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,5 +1,5 @@
 require("oil").setup()
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: open parent directory" })
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil .<CR>", { desc = "Oil: open working dir" })
 
