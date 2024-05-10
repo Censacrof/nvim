665f3ffc44fb6f4339c5b2918f247c64c7638b5a
Francesco Galisi <galisifrancesco@gmail.com>
2024-05-11 00:46:58 +0200

    Migrate telescope

---
+++ lua/censacrof/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	init = function()
+		local builtin = require('telescope.builtin')
+		
+		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Telescope: find files (respects .gitignore)" })
+		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: find git files" })
+		vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Telescope: live grep" })
+		vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = "Telescope: find help" })
+		
+		vim.keymap.set('n', 'gr', function() builtin.lsp_references() end,
+			{ desc = "Telescope: find references", noremap = true, silent = true })
+	end
+}
